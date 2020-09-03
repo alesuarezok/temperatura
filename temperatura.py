ead7c9831df992e6de9efaed331af05351a2dd30
Ales <alesuarez.98@hotmail.com>
2020-09-03 18:11:38 -0300

    Primer commit

---
+++ temperatura.py
@@ -0,0 +1,21 @@
+# El siguiente programa debe poder clasificar la temperatura segun el siguiente criterio
+# como temparatura maxima 53
+# y como minimo -40 en grados celcius
+#(si pasa el maximo o esta por debajo del minimo, Respondera el mensaje "Entrada invalida")
+"""__________________________________________
+            |                                |
+temperatura | denominacion                   |
+____________|________________________________|
+            |		                         |
+  temp < 0  | "Sensacion del clima Helado"   |
+____________|________________________________|
+            |                                |
+0<= temp <15|  "Sensacion del clima Frio"    |    
+____________|________________________________|            
+            |                                |
+15<=temp<25 | "Sensacion del clima gradable" |
+____________|________________________________|            
+            |                                |
+temp >= 25  |  "Sensacion del clima caluroso"|
+____________|________________________________|
+
