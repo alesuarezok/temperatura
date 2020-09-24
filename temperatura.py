2b9de3aed2b4d4b49dd0a5dff5691c91cc1eba9c
amansil3 <amansil3@rosario.gov.ar>
2020-09-23 21:32:00 -0300

    Le agregué un bucle para que no se termine el programa cada vez que un/a user ingrese una entrada inválida: temperaturas inexistentes o cadenas de letras

--- temperatura.py
+++ temperatura.py
@@ -21,28 +21,38 @@ ____________|________________________________|"""
 
 # definicion
 def observar_clima():
-
-# ingreso de valor
-    temperatura=int(input("Ingrese un valor: "))
-# carga de temperatura invalida
-    if(temperatura < -40) or (temperatura > 53):
-        respuesta = "Entrada invalida"
-# temperatura entre los valores -40 y 0
-    elif(temperatura < 0): # -40 > temperatura < 0
-        respuesta = "Sensacion del clima 'Helado'"
-# carga de valores entre 0 y 15
-    elif (temperatura >= 0) and (temperatura < 15): # 0 <= temperatura < 15
-        respuesta = "Sensacion del clima 'Frio'"
-# carga de valores entre 15 y 25
-    elif (temperatura >= 15) and (temperatura < 25): # 15 <= temperatura < 25
-        respuesta = "Sensacion del clima 'Agradable'"
-# carga de valores entre 25 y 32
-    elif (temperatura >= 25) and (temperatura < 32): # 25 <= temperatura < 32 
-        respuesta = "Sensacion del clima 'Caluroso'"
-# carga de valores entre 32 y temperatura maxima (53)
-    elif (temperatura >= 32): # 32 <= temperatura
-        respuesta = "Sensacion del clima 'Muy caluroso'"
-# Imprime la "sensacion termica"
-    print (respuesta)
+    print ("Bienvenido")
+    bandera = True  #hago una bandera para un bucle
+    while (bandera == True): #hago un bucle para validar el input del usuario
+        try:
+            temperatura = int(input("Ingrese un valor: "))     # ingreso de valor
+        except ValueError:     # carga de temperatura no es un número
+            print ("No ingresaste un número!")
+        else:
+            if(temperatura < 0 and temperatura > -40): # -40 > temperatura < 0
+                if (temperatura == 1): #singular para 1 grado
+                    print ("Ingresaste", temperatura, "grado. Sensación del clima 'Helado', ¿de verdad tenes salir?")
+                else:
+                    print ("Ingresaste", temperatura, "grados. Sensación del clima 'Helado, ¿de verdad tenes salir?'")
+                bandera = False
+            # carga de valores entre 0 y 15
+            elif (temperatura >= 0) and (temperatura < 15): # 0 <= temperatura < 15
+                print ("Ingresaste", temperatura, "grados. Sensacion del clima 'Frio'. Está para bufanda y campera")
+                bandera = False
+            # carga de valores entre 15 y 25
+            elif (temperatura >= 15) and (temperatura < 25): # 15 <= temperatura < 25
+                print ("Ingresaste", temperatura, "grados. Sensacion del clima 'Agradable'. Ojalá que puedas disfrutar al aire libre")
+                bandera = False
+            # carga de valores entre 25 y 32
+            elif (temperatura >= 25) and (temperatura < 32): # 25 <= temperatura < 32 
+                print ("Ingresaste", temperatura, "grados. Sensacion del clima 'Caluroso'.")
+                bandera = False
+            # carga de valores entre 32 y temperatura maxima (53)
+            elif (temperatura >= 32 and temperatura <= 53): # 32 <= temperatura
+                print ("Ingresaste", temperatura, "grados. Sensacion del clima 'Muy caluroso, casi sofocante'. Si tenés que salir, ponete protector e hidratate mucho.")
+                bandera = False
+            else:
+                print ("Intentalo nuevamente, las temperaturas ingresadas no son correctas")
+                bandera = True #Ejecuto el bucle hasta que el user ingrese una temperatura menor a 53 y mayor a -40
     
 observar_clima() # Permite que el programa inicie directamente
